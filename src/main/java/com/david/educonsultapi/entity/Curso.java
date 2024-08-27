04605ef20d04fc3cd5479e23b42bb2472a30784b
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 14:09:29 -0500

    feat: Add curso entity

---
+++ src/main/java/com/david/educonsultapi/entity/Curso.java
@@ -0,0 +1,37 @@
+package com.david.educonsultapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "profesor_id")
+    private Profesor profesor;
+
+    @ManyToMany
+    @JoinTable(
+            name = "curso_estudiante",
+            joinColumns = @JoinColumn(name = "curso_id"),
+            inverseJoinColumns = @JoinColumn(name = "estudiante_id")
+    )
+    private List<Estudiante> estudiantes;
+
+
+
+}
