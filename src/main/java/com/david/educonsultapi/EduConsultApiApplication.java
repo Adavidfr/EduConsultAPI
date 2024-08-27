0f0bb8c108d11a2e73de92788e567f48bb07655d
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 11:05:29 -0500

    feat: Add project

---
+++ src/main/java/com/david/educonsultapi/EduConsultApiApplication.java
@@ -0,0 +1,13 @@
+package com.david.educonsultapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduConsultApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduConsultApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/david/educonsultapi/EduConsultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.educonsultapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduConsultApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
