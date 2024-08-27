d6afe30a12c3991147d88487c29252a01c12131c
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 14:11:37 -0500

    feat: Add estudiante not found exception

---
+++ src/main/java/com/david/educonsultapi/exception/EstudianteNotFoundException.java
@@ -0,0 +1,8 @@
+package com.david.educonsultapi.exception;
+
+public class EstudianteNotFoundException extends RuntimeException {
+
+    public EstudianteNotFoundException(String message) {
+        super(message);
+    }
+}
