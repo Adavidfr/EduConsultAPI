731e5ab16b82f8f0b6bc5d716ac5b2aa68c15d9e
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 14:12:46 -0500

    feat: Add profesor not found exception

---
+++ src/main/java/com/david/educonsultapi/exception/ProfesorNotFoundException.java
@@ -0,0 +1,9 @@
+package com.david.educonsultapi.exception;
+
+public class ProfesorNotFoundException extends RuntimeException{
+
+    public ProfesorNotFoundException(String message) {
+        super(message);
+    }
+
+}
