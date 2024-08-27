cf2d02e5897783146e070fb7e81a3f34efcacc41
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 14:12:28 -0500

    feat: Add profesor entity

---
+++ src/main/java/com/david/educonsultapi/entity/Profesor.java
@@ -0,0 +1,29 @@
+package com.david.educonsultapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "profesor")
+    private List<Curso> cursos;
+
+
+}
