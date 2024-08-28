fff7bf3735e110576c1f7e7b7910ec283aa9e451
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 20:31:37 -0500

    refactor: change eliminar service

--- src/main/java/com/david/educonsultapi/service/Impl/ProfesorServiceImpl.java
+++ src/main/java/com/david/educonsultapi/service/Impl/ProfesorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.david.educonsultapi.service.Impl;
 
+import com.david.educonsultapi.entity.Curso;
 import com.david.educonsultapi.entity.Profesor;
 import com.david.educonsultapi.exception.ProfesorNotFoundException;
+import com.david.educonsultapi.repository.CursoRepository;
 import com.david.educonsultapi.repository.ProfesorRepository;
 import com.david.educonsultapi.service.ProfesorService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import java.util.Optional;
 public class ProfesorServiceImpl implements ProfesorService {
 
     private final ProfesorRepository profesorRepository;
+    private final CursoRepository cursoRepository;
 
     @Override
     public Optional<Profesor> findById(Long id) {
@@ -58,9 +61,21 @@ public class ProfesorServiceImpl implements ProfesorService {
 
     @Override
     public void eliminar(Long id) {
-        if (!profesorRepository.existsById(id)) {
+        Optional<Profesor> profesorOptional = profesorRepository.findById(id);
+        if (profesorOptional.isPresent()) {
+            Profesor profesor = profesorOptional.get();
+
+            // Desvincular el profesor de los cursos
+            for (Curso curso : profesor.getCursos()) {
+                curso.setProfesor(null); // Desvincular al profesor del curso
+                cursoRepository.save(curso); // Guardar el curso sin el profesor
+            }
+
+            profesorRepository.delete(profesor); // Eliminar el profesor
+        } else {
             throw new ProfesorNotFoundException("Profesor con id " + id + " no encontrado");
         }
-        profesorRepository.deleteById(id);
     }
+
+
 }
