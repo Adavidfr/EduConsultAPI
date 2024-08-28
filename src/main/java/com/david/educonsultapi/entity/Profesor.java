ad2bfdd2b69826abe2141beb646dfa14c8b334cc
Alex David Frías <adavidfruiz@gmail.com>
2024-08-27 20:30:51 -0500

    refactor: add annotations

--- src/main/java/com/david/educonsultapi/entity/Profesor.java
+++ src/main/java/com/david/educonsultapi/entity/Profesor.java
@@ -1,5 +1,6 @@
 package com.david.educonsultapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,7 +24,6 @@ public class Profesor {
     private String correoElectronico;
 
     @OneToMany(mappedBy = "profesor")
+    @JsonIgnore
     private List<Curso> cursos;
-
-
 }
